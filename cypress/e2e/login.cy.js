53bc2df6229fa2b29b5cf1de71dc9606c7f09c52
Luis Gabriel Herrera <luisgabriel_it@hotmail.com>
2025-06-15 20:08:18 -0300

    #6 Agrego mas pruebas de validación de login (con user incorrecto y contraseña en blanco)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,25 @@
-describe('Login Demo - The Internet', () => {
-    it('Hace login correctamente', ()=>{
-        cy.visit('https://the-internet.herokuapp.com/login');
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPassword!');
-        cy.get('button[type="submit"]').click();
-        cy.contains('You logged into a secure area!').should('be.visible');
-    });
+describe('Pruebas de Login', () => {
+  it('Login exitoso', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+    cy.get('.flash.success').should('contain', 'You logged into a secure area!');
+  });
+
+  it('Login con usuario incorrecto', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('usuario_incorrecto');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+    cy.get('.flash.error').should('contain', 'Your username is invalid!');
+  });
+
+  it('Login con contraseña vacía', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').clear();
+    cy.get('button[type="submit"]').click();
+    cy.get('.flash.error').should('contain', 'Your password is invalid!');
+  });
 });
